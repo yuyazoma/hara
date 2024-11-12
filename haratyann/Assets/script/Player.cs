65ab43e50269bc48f4c3dc85f02b1f3d6f6b119f
yuyazoma <yuyakato2311@icloud.com>
2024-11-12 17:05:30 +0900

    宿題まで

--- haratyann/Assets/script/Player.cs
+++ haratyann/Assets/script/Player.cs
@@ -5,23 +5,21 @@ using TMPro;
 public class Player : MonoBehaviour
 {
     private float moveValue;
-    private float jumpValue; // Declare jumpValue here
+    private float jumpValue;
     public TextMeshProUGUI debugText;
     public int[] sw = new int[3];
     public int[] vol = new int[2];
-    int[] swPre = new int[3]; 
+    int[] swPre = new int[3];
 
-    // Start is called before the first frame update
     void Start()
     {
         moveValue = 0.01f;
-        jumpValue = 5f; // Initialize jumpValue here
+        jumpValue = 5f;
         debugText.text = "debug";
         sw[0] = 1; sw[1] = 1; sw[2] = 1;
-        vol[0] = 2000; vol[1] = 2000;
+        vol[0] = 2300; vol[1] = 2300;
     }
 
-    // Update is called once per frame
     void Update()
     {
         var current = Keyboard.current;
@@ -29,16 +27,40 @@ public class Player : MonoBehaviour
         if (current == null)
             return;
 
-        // Move right
-        if (current.rightArrowKey.isPressed || sw[0] == 0 || vol[0] > 3400)
+        // Right movement
+        if (current.rightArrowKey.isPressed || sw[0] == 0 || vol[0] > 2600)
+        {
             this.transform.position += new Vector3(moveValue, 0f, 0f);
+            if (current.rightArrowKey.isPressed || sw[0] == 0 || vol[0] > 4090)
+                this.transform.position += new Vector3(2 * moveValue, 0f, 0f);
+        }
 
-        // Move left
-        if (current.leftArrowKey.isPressed || sw[1] == 0 || vol[0] < 4000)
+        // Left movement
+        if (current.leftArrowKey.isPressed || sw[1] == 0 || vol[0] < 2200)
+        {
             this.transform.position += new Vector3(-moveValue, 0f, 0f);
+            if (current.leftArrowKey.isPressed || sw[1] == 0 || vol[0] < 1000)
+                this.transform.position += new Vector3(-2 * moveValue, 0f, 0f);
+        }
+
+        // Forward (up) movement along Z-axis
+        if (current.upArrowKey.isPressed || sw[0] == 0 || vol[1] > 2600)
+        {
+            this.transform.position += new Vector3(0f, 0f, moveValue); // ZŽ²•ûŒü‚É‘Oi
+            if (current.upArrowKey.isPressed || sw[0] == 0 || vol[1] > 4090)
+                this.transform.position += new Vector3(0f, 0f, 2 * moveValue);
+        }
+
+        // Backward (down) movement along Z-axis
+        if (current.downArrowKey.isPressed || sw[1] == 0 || vol[1] < 2200)
+        {
+            this.transform.position += new Vector3(0f, 0f, -moveValue); // ZŽ²•ûŒü‚ÉŒã‘Þ
+            if (current.downArrowKey.isPressed || sw[1] == 0 || vol[1] < 1000)
+                this.transform.position += new Vector3(0f, 0f, -2 * moveValue);
+        }
 
         // Jump
-        if (current.zKey.wasPressedThisFrame || sw[2] == 0 && swPre[2] == 1)
+        if (current.zKey.wasPressedThisFrame || (sw[2] == 0 && swPre[2] == 1))
         {
             GetComponent<Rigidbody>().velocity = Vector3.up * jumpValue;
         }
